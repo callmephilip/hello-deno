1b1b776ef814de55833b84fa641248b7c58771f1
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-12-09 18:02:59 +0000

    add proper export bin

--- bin/export.ts
+++ bin/export.ts
@@ -1,5 +1,14 @@
 import { exportNb } from "../export.ts";
 
 if (import.meta.main) {
-  console.log("Exporting notebook... with", exportNb);
+  const args = Deno.args;
+
+  if (args.length !== 1) {
+    console.error(
+      "Usage: deno run jsr:@deno-playground/hello/export your_module.ipynb"
+    );
+    Deno.exit(1);
+  }
+
+  exportNb(args[0]);
 }
